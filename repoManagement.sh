acaebe9d180ae1d6f09849bf3c3e449dcd5ad3ee
KamLan <adouardcamille@hotmail.com>
2018-03-19 14:06:57 +0100

    modification to bulk add & bulk delete

--- repoManagement.sh
+++ repoManagement.sh
@@ -32,7 +32,7 @@ clear
 echo "You have picked the project creation !"
 echo "Please enter the name of the project you want to create"
 read projectName
-curl --header "PRIVATE-TOKEN: UG3RFCZ861_ed8gzSvzc" -X POST "http://172.17.0.5/api/v4/projects?name="$projectName""
+curl --header "PRIVATE-TOKEN: NwDXM3z-8bVG5G5byhkN" -X POST "http://172.17.0.2/api/v4/projects?name="$projectName""
 echo "The project has been created"
 echo "You are now going back to the main menu"
 sleep 2
@@ -42,7 +42,7 @@ exec /home/kam/Courses/GitLab/API-scripts/repoManagement.sh
 "2")
 clear
 echo "You have chosen to see the list of projects on the GitLab server !"
-curl --header "PRIVATE-TOKEN: UG3RFCZ861_ed8gzSvzc" -X GET "http://172.17.0.5/api/v4/projects/" | jq '.'
+curl --header "PRIVATE-TOKEN: NwDXM3z-8bVG5G5byhkN" -X GET "http://172.17.0.2/api/v4/projects/" | jq '.'
 echo "This is the end of the list of projects"
 echo "Press any key to go back"
 read -n 1 x; while read -n 1 -t .1 y; do x="$x$y"; done
@@ -56,7 +56,7 @@ clear
 echo "You have picked the deletion of a project !"
 echo "Please enter the id of the project you want to delete"
 read projectId
-curl --header "PRIVATE-TOKEN: UG3RFCZ861_ed8gzSvzc" -X DELETE "http://172.17.0.5/api/v4/projects/$projectId"
+curl --header "PRIVATE-TOKEN: NwDXM3z-8bVG5G5byhkN" -X DELETE "http://172.17.0.2/api/v4/projects/$projectId"
 echo "The project has been deleted"
 echo "You are now going back to the main menu"
 sleep 2
@@ -68,7 +68,7 @@ clear
 echo "You have picked to check the users for a project"
 echo "Please enter the id of the project"
 read projectId
-curl --header "PRIVATE-TOKEN: UG3RFCZ861_ed8gzSvzc" -X GET "http://172.17.0.5/api/v4/projects/$projectId/members" | jq '.'
+curl --header "PRIVATE-TOKEN: NwDXM3z-8bVG5G5byhkN" -X GET "http://172.17.0.2/api/v4/projects/$projectId/members" | jq '.'
 echo "The is is the end of the members list"
 echo "Press any key to go back"
 read -n 1 x; while read -n 1 -t .1 y; do x="$x$y"; done
@@ -94,9 +94,11 @@ case "$response" in
     exec 4<$textFileName
     while read -u4 t; do
         echo $t
-        curl --header "PRIVATE-TOKEN: UG3RFCZ861_ed8gzSvzc" -X POST "http://172.17.0.5/api/v4/projects?name="$t""
+        curl --header "PRIVATE-TOKEN: NwDXM3z-8bVG5G5byhkN" -X POST "http://172.17.0.2/api/v4/projects?name="$t""
         echo "The project $t has been created"
     done
+    echo "Press any key to go back"
+    read -n 1 x; while read -n 1 -t .1 y; do x="$x$y"; done
     ;;
 
     [nN][oO]|[nN])
@@ -132,9 +134,11 @@ case "$response" in
     exec 4<$textFileName
     while read -u4 t; do
         echo $t
-        curl --header "PRIVATE-TOKEN: UG3RFCZ861_ed8gzSvzc" -X DELETE "http://172.17.0.5/api/v4/projects/$projectId"
+        curl --header "PRIVATE-TOKEN: NwDXM3z-8bVG5G5byhkN" -X DELETE "http://172.17.0.2/api/v4/projects/$t"
         echo "The project $t has been deleted"
     done
+    echo "Press any key to go back"
+    read -n 1 x; while read -n 1 -t .1 y; do x="$x$y"; done
     ;;
 
     [nN][oO]|[nN])
@@ -161,7 +165,7 @@ clear
 echo "This option is not available"
 sleep 2
 clear
-exec /home/kam/COurses/GitLab/API-scritps/repoManagement.sh
+exec /home/kam/Courses/GitLab/API-scritps/repoManagement.sh
 ;;
 
 esac
